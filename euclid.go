d3e5b898ac4d92a29a80e161b32344de5bd1e38c
Philipp Sauter <sauterp@protonmail.com>
2020-03-09 07:44:48 +0900

    function and literature reference

---
+++ euclid.go
@@ -0,0 +1,8 @@
+// Package euclid implements the Euclidean algorithm in its simplest form. The implementation is based on "A Computational Introduction to Number Theory and Algebra" version 2, 2008, chapter 4 "Euclid's algorithm" by Victor Shoup [1].
+package euclid
+
+// GCD computes the Greatest Common Divisor of non-negative integers a and b.
+// On page 76 of [1] you will find a pseudocode.
+func GCD(a, b uint64) (d uint64, err error) {
+	return 0, nil
+}
