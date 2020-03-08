2a6598b29b9a9d19322565c5cab5bc208d7314f6
Philipp Sauter <sauterp@protonmail.com>
2020-03-09 07:48:51 +0900

    ZeroTest

--- euclid_test.go
+++ euclid_test.go
@@ -1,1 +1,14 @@
 package euclid
+
+import "testing"
+
+func ZeroTest(t *testing.T) {
+	d, err := GCD(0, 1)
+	if err != nil || d != 0 {
+		t.Fail()
+	}
+	d, err = GCD(1, 0)
+	if err != nil || d != 0 {
+		t.Fail()
+	}
+}
