b2ec8dc8e90e77d46cb2ce61f1db98fbe81d8e22
Philipp Sauter <sauterp@protonmail.com>
2020-03-12 22:36:52 +0900

    tests

--- euclid_test.go
+++ euclid_test.go
@@ -1,7 +1,11 @@
 package euclid
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
+// TestZero tests GCD for correct behaviour with inputs set to zero.
 func TestZero(t *testing.T) {
 	d, err := GCD(0, 1)
 	if err != nil || d != 1 {
@@ -11,6 +15,41 @@ func TestZero(t *testing.T) {
 	if err != nil || d != 1 {
 		t.Fail()
 	}
+	d, err = GCD(0, 0)
+	if err == nil {
+		t.Fail()
+	}
 }
 
-//func Test(t *testing.T) {
+// TestPrimes tests GCD for all the combinations of the first ten primes.
+func TestPrimes(t *testing.T) {
+	primes := []uint64 {
+		2,
+		3,
+		5,
+		7,
+		11,
+		13,
+		17,
+		19,
+		23,
+		29,
+	}
+	for _, p := range primes {
+		for _, q := range primes {
+			t.Run(fmt.Sprintf("GCD(%d,%d)", p, q), func(t *testing.T) {
+				d, err := GCD(p, q)
+				if err != nil {
+					t.Fail()
+				}
+				if p != q && d != 1 {
+					t.Fail()
+				}
+				if p == q && d != p {
+					t.Fail()
+				}
+			})
+		}
+	}
+}
+
